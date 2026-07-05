c762f805447d7cfb749397d35a2f181890ddc301
Junior27VQ <favq1999@outlook.com>
2026-07-04 19:26:03 -0500

    feat: Data Model: Laptop

---
+++ datos.js
@@ -0,0 +1,8 @@
+const laptop = [
+    {"id": 1, "marca": "HP", "procesador": "Intel Core i5", "memoria": "8 GB", "disco": "1TB"},
+    {"id": 2, "marca": "Lenovo", "procesador": "Intel Core i7", "memoria": "16 GB", "disco": "2TB"},
+    {"id": 3, "marca": "Dell", "procesador": "Intel Core i9", "memoria": "32 GB", "disco": "4TB"},
+    {"id": 4, "marca": "Asus", "procesador": "AMD Ryzen 7", "memoria": "16 GB", "disco": "1TB"}
+];
+
+module.exports = laptop; // Esto permite importar estos datos en otros archivos
