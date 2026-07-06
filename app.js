b81eff929729a12b13717b4498b28f84de384c5f
Junior27VQ <favq1999@outlook.com>
2026-07-06 08:41:40 -0500

    feat: Recovering a laptop

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.post('/laptops', (req, res) => {
     laptop.push(nuevaLaptop); 
   res.send(nuevaLaptop);
 });
+// Recuperar una laptop por ID
+app.get('/laptops/:id', (req, res) => {
+    const id = req.params.id; // Obtener el ID de la laptop desde los parámetros de la ruta
+    console.log(`Obteniendo laptop con ID: ${id}`);
+    const laptopEncontrada = laptop.find(lap => lap.id == id); // Buscar la laptop por ID
+    if (laptopEncontrada) {
+        res.send(laptopEncontrada); // Enviar la laptop encontrada como respuesta
+    } else {
+        res.status(404).send('Laptop no encontrada'); //Enviamos un mensaje de error si no se encuentra la laptop
+    }
+});
 
 app.listen(port, () => {
   console.log(`Servidor funcionando en http://localhost:${port}`);
