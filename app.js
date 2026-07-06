c57be3caa36ba8413d1e5fca968bdbe5078e1f77
Junior27VQ <favq1999@outlook.com>
2026-07-06 08:36:13 -0500

    feat: http post/laptops implemented

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const laptop = require('./datos.js');
+
+app.use(express.json()); 
+
+//Crear una laptop
+app.post('/laptops', (req, res) => {
+    req.body.id = laptop.length + 1;
+    const nuevaLaptop = req.body; 
+    laptop.push(nuevaLaptop); 
+  res.send(nuevaLaptop);
+});
 
 app.listen(port, () => {
   console.log(`Servidor funcionando en http://localhost:${port}`);
